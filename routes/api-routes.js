06da8d308fedb920d66ec3c45b1f334e81a9990c
DrummD <drummd@rocketmail.com>
2022-09-22 18:24:49 -0700

    updated

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,73 +2,40 @@ const fs = require('fs');
 const path = require('path');
 let uniqid = require("uniqid")
 const db = require("../db/db.json")
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const router = require('express').Router()
 
-const router = require("express").Router;
-
-app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-    );
-
-//8
-app.get('/api', (req, res) => {
+//
+router.get('/notes', (req, res) => {
     console.info(`${req.method} request received for notes`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
 //------------------------
 
-app.post('/api', (req, res) => {
+router.post('/notes', (req, res) => {
     console.info(`${req.method} request received to add note`);
   
-    const { title, text, id } = req.body;
+    const { title, text } = req.body;
   
     if (req.body) {
       const newNote = {
-        title: req.body.title,
-        text: req.body.text,
+        title,
+        text,
         id: uniqid()
       };
   
       readAndAppend(newNote, './db/db.json');
-      res.json(`Tip added successfully 🚀`);
+      res.json(`Note added successfully 🚀`);
     } else {
-      res.error('Error in adding tip');
+      res.error('Error in adding notes');
     }
   });
   
-  // GET Route for retrieving all the feedback
-  app.get('/api', (req, res) => {
-    console.info(`${req.method} request received for notes`);
   
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-  });
+
   
-  // POST Route for submitting feedback
-  app.post('/api', (req, res) => {
-    console.info(`${req.method} request received to submit notes`);
-  
-    
-    const { title, text, id } = req.body;
-  
-    
-    if (title && text && id) {
-      const newFeedback = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uniqid()   
-      };
-  
-      readAndAppend(newFeedback, './db/db.json');
-        const response = {
-            status: 'success',
-            body: newNote,
-      };
-  
-      res.json(response);
-    } else {
-      res.json('Error in posting note');
-    }
-  });
+
 
 
 
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require("path");
 const fs = require("fs");
 const htmlRoutes = require("./routes/html-routes")
-const apiRoutes = require("./routes/routes")
+const apiRoutes = require("./routes/api-routes")
 const app = express();
-const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
